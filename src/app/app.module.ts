04d42a5a7315089f1cb8c091e4975d79768efc93
DESKTOP-O0UCAST\itsix <nicoleta260902@gmail.com>
2022-11-24 09:13:36 -0800

    My first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultButtonComponent } from './component/default-button/default-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/default-button/default-button.component.ts
@@ -0,0 +1,11 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-default-button',
+  templateUrl: './default-button.component.html',
+  styleUrls: ['./default-button.component.css']
+})
+export class DefaultButtonComponent {
+  @Input()buttonTitle:string="defaultTitle";
+  
+}
